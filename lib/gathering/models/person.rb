a4451f8d7f91798e045c65f20e6ef8fbb3c131d2
Andy Nicholson <andrew@anicholson.net>
2014-12-27 16:53:51 +1030

    Person <—> Team many-to-many relationship
    * Find members of team
    * Find teams a person's a member of

--- lib/gathering/models/person.rb
+++ lib/gathering/models/person.rb
@@ -2,6 +2,10 @@ module Gathering
   class Person
     include Lotus::Entity
 
-    self.attributes = :name, :contact_number, :teams
+    self.attributes = :name, :contact_number
+
+    def teams
+      TeamMembershipRepository.teams_for_person(self)
+    end
   end
 end
--- lib/gathering/models/team.rb
+++ lib/gathering/models/team.rb
@@ -2,5 +2,10 @@ module Gathering
   class Team
     include Lotus::Entity
     self.attributes = :name, :people, :leader
+
+    def people
+      TeamMembershipRepository.members_of_team(self)
+    end
+
   end
 end

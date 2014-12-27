563edd501c917f40074c3bc0d09c3ab846c36166
Andy Nicholson <andrew@anicholson.net>
2014-12-27 17:20:37 +1030

    Find the leader of a team

--- lib/gathering/models/team.rb
+++ lib/gathering/models/team.rb
@@ -1,11 +1,14 @@
 module Gathering
   class Team
     include Lotus::Entity
-    self.attributes = :name, :people, :leader
+    self.attributes = :name, :people, :leader_id
 
     def people
       TeamMembershipRepository.members_of_team(self)
     end
 
+    def leader
+      PersonRepository.find(self.leader_id)
+    end
   end
 end
